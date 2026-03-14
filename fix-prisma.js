529915e0e05ff3ff60b63e14e5766f9f6f6b5e12
ABDELRAHMAN ELSAYED <abdoelhosin250@gmail.com>
2026-03-15 01:10:30 +0300

    clean up: remove temporary scripts

--- fix-prisma.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('prisma/schema.prisma', 'utf8');
-content = content.replace(/,\s*where:\s*raw\("[^"]+"\)/g, '');
-fs.writeFileSync('prisma/schema.prisma', content);
